3afc481d449dce02777a7009e3749bd0efa9e8e6
StarAres1 <titanolimpa5@gmail.com>
2025-04-16 22:08:45 +0300

    Logout, hash and unique field in model of User
    Добавлена страничка с профилем, где можно посмотреть данные о текущем юзере
    
    Добавлено хэширование паролей
    
    Добавлен Log out из профиля
    
    Изменена модель User. Теперь нельзя создать двух пользователей с одинаковыми или почтами. И измнен тип поля Id с int на uint

---
+++ Migrations/20250416185104_AddUniqueIndexesToUser.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FranchiseVerse.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexesToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "user",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_Email",
+                table: "user",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_UserName",
+                table: "user",
+                column: "UserName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_user_Email",
+                table: "user");
+
+            migrationBuilder.DropIndex(
+                name: "IX_user_UserName",
+                table: "user");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "user",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+    }
+}
